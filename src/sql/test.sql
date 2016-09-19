78307cfdf81455c1bae2e7e7e478637713b2e89a
konamione <103349652@qq.com>
2016-09-19 17:51:20 +0800

    update bate 1.1

--- src/sql/test.sql
+++ src/sql/test.sql
@@ -1,24 +1,54 @@
-/*
-Navicat MySQL Data Transfer
+-- MySQL dump 10.13  Distrib 5.5.24, for Win32 (x86)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.5.24-log
 
-Source Server         : localhost
-Source Server Version : 50524
-Source Host           : localhost:3306
-Source Database       : test
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Target Server Type    : MYSQL
-Target Server Version : 50524
-File Encoding         : 65001
+--
+-- Table structure for table `t_cash`
+--
 
-Date: 2016-09-15 17:42:01
-*/
+DROP TABLE IF EXISTS `t_cash`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_cash` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `res` varchar(500) DEFAULT NULL,
+  `remark` varchar(2000) DEFAULT NULL,
+  `money` decimal(10,2) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  `createid` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Dumping data for table `t_cash`
+--
 
--- ----------------------------
--- Table structure for `t_goods`
--- ----------------------------
+LOCK TABLES `t_cash` WRITE;
+/*!40000 ALTER TABLE `t_cash` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_cash` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_goods`
+--
+
 DROP TABLE IF EXISTS `t_goods`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_goods` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goodsname` varchar(200) DEFAULT NULL,
@@ -31,17 +61,19 @@ CREATE TABLE `t_goods` (
   `modifyid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_goods
--- ----------------------------
-INSERT INTO `t_goods` VALUES ('1', '一品香', '斤', '10.50', '20.50', '2016-09-15 15:34:36', null, '1', null);
-INSERT INTO `t_goods` VALUES ('2', '二品香', '斤', '20.50', '40.50', '2016-09-15 15:59:42', '2016-09-15 15:59:42', '1', '1');
+--
+-- Dumping data for table `t_goods`
+--
 
--- ----------------------------
--- Table structure for `t_inventory`
--- ----------------------------
+--
+-- Table structure for table `t_inventory`
+--
+
 DROP TABLE IF EXISTS `t_inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_inventory` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goodsid` bigint(20) DEFAULT NULL,
@@ -52,16 +84,19 @@ CREATE TABLE `t_inventory` (
   `createtime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_inventory
--- ----------------------------
-INSERT INTO `t_inventory` VALUES ('2', '1', '5000', 'buy', 'dfgdfg', '1', '2016-09-15 16:30:16');
+--
+-- Dumping data for table `t_inventory`
+--
 
--- ----------------------------
--- Table structure for `t_sell`
--- ----------------------------
+--
+-- Table structure for table `t_sell`
+--
+
 DROP TABLE IF EXISTS `t_sell`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_sell` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goodsid` bigint(20) DEFAULT NULL,
@@ -72,17 +107,27 @@ CREATE TABLE `t_sell` (
   `remark` varchar(2000) DEFAULT NULL,
   `createTime` datetime DEFAULT NULL,
   `createid` bigint(20) DEFAULT NULL,
+  `custname` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_sell
--- ----------------------------
+--
+-- Dumping data for table `t_sell`
+--
 
--- ----------------------------
--- Table structure for `t_storage`
--- ----------------------------
+LOCK TABLES `t_sell` WRITE;
+/*!40000 ALTER TABLE `t_sell` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_sell` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_storage`
+--
+
 DROP TABLE IF EXISTS `t_storage`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_storage` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `goodsid` bigint(20) DEFAULT NULL,
@@ -90,15 +135,24 @@ CREATE TABLE `t_storage` (
   `createtime` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_storage
--- ----------------------------
+--
+-- Dumping data for table `t_storage`
+--
 
--- ----------------------------
--- Table structure for `t_user`
--- ----------------------------
+LOCK TABLES `t_storage` WRITE;
+/*!40000 ALTER TABLE `t_storage` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_storage` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_user`
+--
+
 DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `t_user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `username` varchar(50) CHARACTER SET utf8 DEFAULT NULL,
@@ -112,8 +166,25 @@ CREATE TABLE `t_user` (
   `modifyid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of t_user
--- ----------------------------
-INSERT INTO `t_user` VALUES ('1', 'admin', 'admin', '系统管理员', '18888888888', '宇宙地球中国', '2016-09-14 10:05:49', '2016-09-14 10:13:03', '1', '1');
+--
+-- Dumping data for table `t_user`
+--
+
+LOCK TABLES `t_user` WRITE;
+/*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+INSERT INTO `t_user` VALUES (1,'admin','admin','系统管理员','18888888888','宇宙地球中国','2016-09-14 10:05:49','2016-09-14 10:13:03',1,1);
+/*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-19 12:09:22
